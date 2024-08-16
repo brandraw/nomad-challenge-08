f605a40e0b9465c3ed214485da2a795f543243e5
창욱 <changwook@chang-ug-ui-MacBookAir.local>
2024-08-16 13:54:27 +0900

    challenge zod

--- app/(auth)/(routes)/login/actions.ts
+++ app/(auth)/(routes)/login/actions.ts
@@ -4,65 +4,65 @@ import { db } from "@/lib/db";
 import { z } from "zod";
 import bcrypt from "bcrypt";
 
-const formSchema = z
-  .object({
-    username: z
-      .string()
-      .min(3)
-      .max(10)
-      .trim()
-      .toLowerCase()
-      .refine(async (username) => {
-        const user = await db.user.findUnique({
-          where: {
-            username,
-          },
-          select: {
-            id: true,
-          },
-        });
+const PasswordRegex = /^(?=.*\d).+$/;
 
-        return Boolean(user);
-      }, "Username Not Found"),
-    email: z
-      .string()
-      .email()
-      .refine(async (email) => {
-        const user = await db.user.findUnique({
-          where: {
-            email,
-          },
-          select: {
-            id: true,
-          },
-        });
+const formSchema = z.object({
+  username: z.string().min(5).max(10).trim().toLowerCase(),
+  // .refine(async (username) => {
+  //   const user = await db.user.findUnique({
+  //     where: {
+  //       username,
+  //     },
+  //     select: {
+  //       id: true,
+  //     },
+  //   });
 
-        return Boolean(user);
-      }, "Email Not Found"),
-    password: z.string(),
-  })
-  .superRefine(async ({ email, password }, ctx) => {
-    const user = await db.user.findUnique({
-      where: {
-        email,
-      },
-      select: {
-        password: true,
-      },
-    });
+  //   return Boolean(user);
+  // }, "Username Not Found")
+  email: z
+    .string()
+    .email()
+    .refine((email) => email.includes("@zod.com"), "Not Zexy Zod"),
+  // .refine(async (email) => {
+  //   const user = await db.user.findUnique({
+  //     where: {
+  //       email,
+  //     },
+  //     select: {
+  //       id: true,
+  //     },
+  //   });
 
-    const checkPassword = await bcrypt.compare(password, user?.password || "");
-    if (!checkPassword) {
-      ctx.addIssue({
-        code: "custom",
-        message: "Wrong password",
-        path: ["password"],
-        fatal: true,
-      });
+  //   return Boolean(user);
+  // }, "Email Not Found")
+  password: z
+    .string()
+    .min(10)
+    .regex(PasswordRegex, "Password should contain at least one number"),
+});
+// .superRefine(async ({ email, password }, ctx) => {
+//   const user = await db.user.findUnique({
+//     where: {
+//       email,
+//     },
+//     select: {
+//       password: true,
+//     },
+//   });
 
-      return z.NEVER;
-    }
-  });
+//   const checkPassword = await bcrypt.compare(password, user?.password || "");
+//   if (!checkPassword) {
+//     ctx.addIssue({
+//       code: "custom",
+//       message: "Wrong password",
+//       path: ["password"],
+//       fatal: true,
+//     });
+
+//     return z.NEVER;
+//   }
+// });
 
 export async function handleLogin(_: any, formData: FormData) {
   await new Promise((r) => setTimeout(r, 500));
